58d0c22802cb020cad628ec70857c143aab0223d
Mingoo Park <sogoodmg@gmail.com>
2023-11-28 11:23:29 +0900

    코드 작성 완료
    코드를 작성 완료하였습니다.

---
+++ Assets/Scripts/C04/ArithmaticOperators.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArithmaticOperators : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 111 + 222;
+        Debug.Log($"a : {a}");
+
+        int b = a - 100;
+        Debug.Log($"b : {b}");
+
+        int c = b * 10;
+        Debug.Log($"c : {c}");
+
+        double d = c / 6.3;
+        Debug.Log($"d : {d}");
+
+        Debug.Log($"22 / 7 = {22 / 7}({22 % 7})");
+    }
+}
